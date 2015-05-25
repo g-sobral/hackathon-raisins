3e1997a6a808ec44b1008e6cfed2227db354d2bd
Gabriel Sobral <gasan.sobral@gmail.com>
2015-05-25 10:33:01 +0000

    final!

--- web/freeboard/alarm.py
+++ web/freeboard/alarm.py
@@ -1,13 +1,15 @@
 import sched, time
 from datetime import datetime, timedelta
 import urllib2
+from embedded import Edison
+import hackeld_bot as bot
 
 s = sched.scheduler(time.time, time.sleep)
 ed = Edison()
 
 def notify(): 
 	urllib2.urlopen("http://192.168.1.129:5000/set/alarm/1/1").read()
-	
+	bot.send_telegram_message("O Sr Edison deve tomar o remedio!")
 	ed.turn_led_on('r')
 	# Check button
 	check_alarm()
@@ -18,20 +20,22 @@ def check_alarm():
 	# Keep pooling button
 	start = time.time()
 	prev = start
-	while not read_button():
+	while not ed.read_button():
 		now = time.time()
 		late = now - start
 		ctrl = now - prev
-		prev = now 
 		if ctrl  > 1:
 			urllib2.urlopen("http://192.168.1.129:5000/set/alarm/1/" + 
 str(int(late))).read()
+			prev = now
 
 	ed.turn_led_off('r')
-	urllib2.urlopen("http://192.168.1.129:5000/set/alarm/1/-1").read()
+	bot.send_telegram_message("Remedio Tomado Direitinho ;)")
+	urllib2.urlopen("http://192.168.1.129:5000/disable/alarm/1").read()
 	return
 
 def start_alarm(delay):
+	urllib2.urlopen("http://192.168.1.129:5000/set/alarm/1/-1").read()
 	s.enter(delay, 1, notify, ())
 	s.run()
 	return
